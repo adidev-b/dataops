e6fe9773892ebb169d5e3ac01672eb4ace713898
Bibhu.Panda <bibhupanda91@gmail.com>
2020-08-07 13:31:01 +0530

    20200807

---
+++ DA.py
@@ -0,0 +1,15 @@
+# import libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+import scipy
+
+# %matplotlib inline
+
+
+sales = pd.read_csv('C:\\Users\\Bibhu.Panda\\Documents\\demopyproject\\sales_data_sample.csv', parse_dates=['Date'])
+
+
+
